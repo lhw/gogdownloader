d45eb9984a990b4eeda064891155d81233466df1
Lennart Weller <lhw@ring0.de>
2012-07-28 23:37:12 +0200

    found all libs

--- cmake/FindProtobufC.cmake
+++ cmake/FindProtobufC.cmake
@@ -1,20 +1,19 @@
-# - Try to find ProtoBufC
+# - Try to find libprotobuf-c
 # Once done this will define
-#  PROTOBUFC_FOUND - system has ProtoBufC
-#  PROTOBUFC_INCLUDE_DIR - the ProtoBufC include directory
-#  PROTOBUFC_LIBRARIES - Link these to use ProtoBufC
-#  PROTOBUFC_DEFINITIONS - Compiler switches required for using ProtoBufC
-
+#  LIBPROTOBUFC_FOUND - System has libprotobuf-c
+#  LIBPROTOBUFC_INCLUDE_DIRS - The libprotobuf-c include directories
+#  LIBPROTOBUFC_LIBRARIES - The libraries needed to use libprotobuf-c
+#  LIBPROTOBUFC_DEFINITIONS - Compiler switches required for using libprotobuf-c
 
 find_package(PkgConfig)
 pkg_check_modules(PC_LIBPROTOBUFC_QUIET libprotobuf-c)
 set(LIBPROTOBUFC_DEFINITIONS ${PC_LIBPROTOBUFC_CFLAGS_OTHER})
 
-find_path(LIBPROTOBUFC_INCLUDE_DIR google/protobuf-c/protobuf-c.h
+find_path(LIBPROTOBUFC_INCLUDE_DIR google/protobuf-c/protobuf-c.h 
 	HINTS ${PC_LIBPROTOBUFC_INCLUDEDIR} ${PC_LIBPROTOBUFC_INCLUDE_DIRS}
 	PATH_SUFFIXES libprotobuf-c)
 
-find_library(LIBPROTOBUFC_LIBRARY NAMES libprotobuf-c
+find_library(LIBPROTOBUFC_LIBRARY NAMES protobuf-c 
 	HINTS ${PC_LIBPROTOBUFC_LIBDIR}
 	${PC_LIBPROTOBUFC_LIBRARY_DIRS})
 
@@ -22,6 +21,6 @@ set(LIBPROTOBUFC_LIBRARIES ${LIBPROTOBUFC_LIBRARY})
 set(LIBPROTOBUFC_INCLUDE_DIRS ${LIBPROTOBUFC_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(liboauth DEFAULT_MSG
+find_package_handle_standard_args(libprotobuf-c DEFAULT_MSG
 	LIBPROTOBUFC_LIBRARY LIBPROTOBUFC_INCLUDE_DIR)
 mark_as_advanced(LIBPROTOBUFC INCLUDE_DIR LIBPROTOBUFC_LIBRARY)
